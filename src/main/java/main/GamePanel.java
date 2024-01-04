1e65a3de56490c645f07e16bd22a37842816f0b4
Corey Jordan <113132164+CoreyJordan@users.noreply.github.com>
2024-01-03 20:12:25 -0500

    Add KeyHandler and set listeners
    This should listen for wasd inputs and respond
    accordingly. We've implemented a temporary 'player'
    in the form of a white rectangle.
    
    Without time, the rectangle quickly zooms off the
    screen.

--- src/main/java/main/GamePanel.java
+++ src/main/java/main/GamePanel.java
@@ -1,6 +1,6 @@
 package main;
 
-import utils.Globals;
+import utils.KeyHandler;
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,11 +10,19 @@ import static utils.Globals.*;
 public class GamePanel extends JPanel implements Runnable{
 
     Thread gameThread;
+    KeyHandler keyH = new KeyHandler();
+
+    private int playerX = 100;
+    private int playerY = 100;
+    private int playerSpeed = 4;
+
 
     public GamePanel() {
         setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         setBackground(Color.BLACK);
         setDoubleBuffered(true);
+        addKeyListener(keyH);
+        setFocusable(true);
     }
 
     /**
@@ -25,8 +33,47 @@ public class GamePanel extends JPanel implements Runnable{
         gameThread.start();
     }
 
+    /**
+     * Game loop for the main game panel.
+     */
     @Override
     public void run() {
+
+        while (gameThread != null) {
+
+            update();
+
+            repaint();
+
+        }
+
+    }
+
+    /**
+     * Updates the game model each loop.
+     */
+    public void update() {
+        if (keyH.upPressed) {
+            playerY -= playerSpeed;
+        } else if (keyH.downPressed) {
+            playerY += playerSpeed;
+        } else if (keyH.leftPressed) {
+            playerX -= playerSpeed;
+        } else if (keyH.rightPressed) {
+            playerX += playerSpeed;
+        }
+    }
+
+    /**
+     * Draws components in the game loop through the repaint() method.
+     * @param g the <code>Graphics</code> object to protect
+     */
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2D = (Graphics2D) g;
+        g2D.setColor(Color.WHITE);
+        g2D.fillRect(playerX, playerY, TILE_SIZE, TILE_SIZE);
+        g2D.dispose();
 
     }
 }
---
+++ src/main/java/utils/KeyHandler.java
@@ -0,0 +1,68 @@
+package utils;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+    public boolean upPressed;
+    public boolean downPressed;
+    public boolean leftPressed;
+    public boolean rightPressed;
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    /**
+     * Listens for keys to be pressed and toggles to relevant boolean.
+     * @param e the event to be processed
+     */
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if (code == KeyEvent.VK_W) {
+            upPressed = true;
+        }
+
+        if (code == KeyEvent.VK_S) {
+            downPressed = true;
+        }
+
+        if (code == KeyEvent.VK_A) {
+            leftPressed = true;
+        }
+
+        if (code == KeyEvent.VK_D) {
+            rightPressed = true;
+        }
+
+    }
+
+    /**
+     * Listens for keys to be released and toggles to relevant boolean.
+     * @param e the event to be processed
+     */
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if (code == KeyEvent.VK_W) {
+            upPressed = false;
+        }
+
+        if (code == KeyEvent.VK_S) {
+            downPressed = false;
+        }
+
+        if (code == KeyEvent.VK_A) {
+            leftPressed = false;
+        }
+
+        if (code == KeyEvent.VK_D) {
+            rightPressed = false;
+        }
+    }
+}
